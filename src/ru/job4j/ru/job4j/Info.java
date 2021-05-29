7338ccbbede20d6f8cae8308b8c7e674050e459c
denis.kotov <enzaden@gmail.com>
2021-05-29 17:03:39 +0300

    3.2. Фиксация изменений на github. [#243564]

--- src/ru/job4j/ru/job4j/Info.java
+++ src/ru/job4j/ru/job4j/Info.java
@@ -2,6 +2,8 @@ package ru.job4j.ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("29.05.2021");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 9 = 9");
     }
 }
